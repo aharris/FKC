7f7db343eb672530f0630e1714c81e8266d1aa94
Ashton Harris <ahdesignsonline@gmail.com>
2011-08-05 20:55:18 -0400

    FKC initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fkc::Application.config.secret_token = 'da96f6cc89f73927ed63a36060b9b65c37050168b7e7e3a07667ed4454004b4f9809a75fc890bd0a3e93c486c1d45a7a37b6bb5442a102bc6192c21cf281a04a'
