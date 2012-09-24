ce509f37a915c5090edb46160cccd417d551051f
Ashton Harris <ahdesignsonline@gmail.com>
2012-09-23 21:41:25 -0400

    edits config

--- config/application.rb
+++ config/application.rb
@@ -4,14 +4,14 @@ require 'rails/all'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require *Rails.groups(:assets => %w(development test))
+  # Bundler.require *Rails.groups(:assets => %w(development test))
   # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
+  Bundler.require(:default, :assets, Rails.env)
 end
 
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env) if defined?(Bundler)
+# Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module Fkc
   class Application < Rails::Application
@@ -51,7 +51,5 @@ module Fkc
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
-    
-    config.assets.precompile += ['application.css.sass']
   end
 end
