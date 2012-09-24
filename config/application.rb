46c0b1a9990399d3e6b1f0722c30d22b855d92b0
Ashton Harris <ahdesignsonline@gmail.com>
2012-09-23 21:15:07 -0400

    does not precompile assets

--- config/application.rb
+++ config/application.rb
@@ -51,5 +51,6 @@ module Fkc
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+    config.assets.initialize_on_precompile = false
   end
 end
