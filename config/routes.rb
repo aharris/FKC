ea6fabbf5aa28392e89aca2da4a90c00aedfc291
Ashton Harris <ahdesignsonline@gmail.com>
2012-10-27 19:04:27 -0400

    [close #5] Change title of first key to our story

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Fkc::Application.routes.draw do
   root :to => 'pages#home', :as => 'home'
   get 'about' => 'pages#about'
-  get 'firstkey' => 'pages#firstkey'
+  get 'ourstory' => 'pages#ourstory'
   get 'projects' => 'pages#projects'
   get 'music' => 'pages#music'
   get 'testimonials' => 'pages#testimonials'
