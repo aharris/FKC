c1e03e9be6701c1ccd89054d986749bf4dfcebd1
Ashton Harris <ahdesignsonline@gmail.com>
2012-10-27 18:42:46 -0400

    [Issue #2] Add te] Add testimonial page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Fkc::Application.routes.draw do
   root :to => 'pages#home', :as => 'home'
-  get 'about' => 'pages#about' 
+  get 'about' => 'pages#about'
   get 'firstkey' => 'pages#firstkey'
   get 'projects' => 'pages#projects'
   get 'music' => 'pages#music'
-  
+  get 'testimonials' => 'pages#testimonials'
+
   get 'requests' => 'pages#requests'
  end
