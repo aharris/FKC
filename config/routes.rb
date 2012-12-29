9b64ad867477bf481cf168daff5f7c9ed4586be2
Ashton Harris <ahdesignsonline@gmail.com>
2012-12-28 23:14:57 -0500

    [Issue #21] Add Redirect to Reverb Nation

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Fkc::Application.routes.draw do
   root :to => 'pages#home', :as => 'home'
   get 'about' => 'pages#about'
   get 'ourstory' => 'pages#ourstory'
-  get 'projects' => 'pages#projects'
+  # get 'projects' => 'pages#projects'
+
+  # ----------------------
+  # Temporary Redirect ---
+  # ----------------------
+  match "/projects" => redirect("http://www.ReverbNation.com/FirstKeyCreations"), :as => :projects
+
   get 'music' => 'pages#music'
   get 'testimonials' => 'pages#testimonials'
 
